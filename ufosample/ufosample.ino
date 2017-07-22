c9ebc539bd27241214c74d1eed8070bbf9dc9192
Michael H. Oshita <ijinpublic+github@gmail.com>
2017-07-22 18:58:35 +0900

    neopixel support

--- ufosample/ufosample.ino
+++ ufosample/ufosample.ino
@@ -1,25 +1,38 @@
-#include <Servo.h> 
+#include <Servo.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(35, PIN, NEO_GRB + NEO_KHZ800);
+
 Servo servo;        //Servoオブジェクトを作成
  
 void setup() 
 { 
   servo.attach(9);  //D9ピンをサーボの信号線として設定
   servo.write(0); // サーボの角度を0°に設定
+  strip.begin();
+  strip.show();
 } 
  
 void loop() 
 { 
-slowDown(360);
-slowUp(0);
+  
+  //rainbow(5);
+  slowDown(360);
+  slowUp(0);
+  //theaterChaseRainbow(25);
+  //theaterChase(strip.Color(127, 127, 127), 50);
 }
+
 void slowDown(int pos){
   while(pos>0){
     servo.write(pos);
-    delay(15); 
+    delay(15);
     pos--;
   }
-    delay(3000); 
+  delay(3000);
 }
+
 void slowUp(int pos){
   while(pos<360){
     servo.write(pos);
@@ -28,4 +41,71 @@ void slowUp(int pos){
   }
   delay(5000); 
 }
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
 
