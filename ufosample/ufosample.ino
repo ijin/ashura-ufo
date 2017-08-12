57e2e8747347b8a1be5bc81cbd8a55fdc3be9211
Michael H. Oshita <ijinpublic+github@gmail.com>
2017-08-12 23:57:21 +0900

    try multi process

--- ufosample/ufosample.ino
+++ ufosample/ufosample.ino
@@ -1,28 +1,482 @@
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_TiCoServo.h>
+
 
 #define PIN 6
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(35, PIN, NEO_GRB + NEO_KHZ800);
 
-Servo servo;        //Servoオブジェクトを作成
+//Servo servo;        //Servoオブジェクトを作成
+Adafruit_TiCoServo servo;
+
+
+// Pattern types supported:
+enum  pattern { NONE, RAINBOW_CYCLE, THEATER_CHASE, COLOR_WIPE, SCANNER, FADE };
+// Patern directions supported:
+enum  direction { FORWARD, REVERSE };
+
+// NeoPattern Class - derived from the Adafruit_NeoPixel class
+class NeoPatterns : public Adafruit_NeoPixel
+{
+    public:
  
+    // Member Variables:  
+    pattern  ActivePattern;  // which pattern is running
+    direction Direction;     // direction to run the pattern
+    
+    unsigned long Interval;   // milliseconds between updates
+    unsigned long lastUpdate; // last update of position
+    
+    uint32_t Color1, Color2;  // What colors are in use
+    uint16_t TotalSteps;  // total number of steps in the pattern
+    uint16_t Index;  // current step within the pattern
+    
+    void (*OnComplete)();  // Callback on completion of pattern
+    
+    // Constructor - calls base-class constructor to initialize strip
+    NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type, void (*callback)())
+    :Adafruit_NeoPixel(pixels, pin, type)
+    {
+        OnComplete = callback;
+    }
+    
+    // Update the pattern
+    void Update()
+    {
+        if((millis() - lastUpdate) > Interval) // time to update
+        {
+            lastUpdate = millis();
+            switch(ActivePattern)
+            {
+                case RAINBOW_CYCLE:
+                    RainbowCycleUpdate();
+                    break;
+                case THEATER_CHASE:
+                    TheaterChaseUpdate();
+                    break;
+                case COLOR_WIPE:
+                    ColorWipeUpdate();
+                    break;
+                case SCANNER:
+                    ScannerUpdate();
+                    break;
+                case FADE:
+                    FadeUpdate();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+  
+    // Increment the Index and reset at the end
+    void Increment()
+    {
+        if (Direction == FORWARD)
+        {
+           Index++;
+           if (Index >= TotalSteps)
+            {
+                Index = 0;
+                if (OnComplete != NULL)
+                {
+                    OnComplete(); // call the comlpetion callback
+                }
+            }
+        }
+        else // Direction == REVERSE
+        {
+            --Index;
+            if (Index <= 0)
+            {
+                Index = TotalSteps-1;
+                if (OnComplete != NULL)
+                {
+                    OnComplete(); // call the comlpetion callback
+                }
+            }
+        }
+    }
+    
+    // Reverse pattern direction
+    void Reverse()
+    {
+        if (Direction == FORWARD)
+        {
+            Direction = REVERSE;
+            Index = TotalSteps-1;
+        }
+        else
+        {
+            Direction = FORWARD;
+            Index = 0;
+        }
+    }
+    
+    // Initialize for a RainbowCycle
+    void RainbowCycle(uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = RAINBOW_CYCLE;
+        Interval = interval;
+        TotalSteps = 255;
+        Index = 0;
+        Direction = dir;
+    }
+    
+    // Update the Rainbow Cycle Pattern
+    void RainbowCycleUpdate()
+    {
+        for(int i=0; i< numPixels(); i++)
+        {
+            setPixelColor(i, Wheel(((i * 256 / numPixels()) + Index) & 255));
+        }
+        show();
+        Increment();
+    }
+ 
+    // Initialize for a Theater Chase
+    void TheaterChase(uint32_t color1, uint32_t color2, uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = THEATER_CHASE;
+        Interval = interval;
+        TotalSteps = numPixels();
+        Color1 = color1;
+        Color2 = color2;
+        Index = 0;
+        Direction = dir;
+   }
+    
+    // Update the Theater Chase Pattern
+    void TheaterChaseUpdate()
+    {
+        for(int i=0; i< numPixels(); i++)
+        {
+            if ((i + Index) % 3 == 0)
+            {
+                setPixelColor(i, Color1);
+            }
+            else
+            {
+                setPixelColor(i, Color2);
+            }
+        }
+        show();
+        Increment();
+    }
+ 
+    // Initialize for a ColorWipe
+    void ColorWipe(uint32_t color, uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = COLOR_WIPE;
+        Interval = interval;
+        TotalSteps = numPixels();
+        Color1 = color;
+        Index = 0;
+        Direction = dir;
+    }
+    
+    // Update the Color Wipe Pattern
+    void ColorWipeUpdate()
+    {
+        setPixelColor(Index, Color1);
+        show();
+        Increment();
+    }
+    
+    // Initialize for a SCANNNER
+    void Scanner(uint32_t color1, uint8_t interval)
+    {
+        ActivePattern = SCANNER;
+        Interval = interval;
+        TotalSteps = (numPixels() - 1) * 2;
+        Color1 = color1;
+        Index = 0;
+    }
+ 
+    // Update the Scanner Pattern
+    void ScannerUpdate()
+    { 
+        for (int i = 0; i < numPixels(); i++)
+        {
+            if (i == Index)  // Scan Pixel to the right
+            {
+                 setPixelColor(i, Color1);
+            }
+            else if (i == TotalSteps - Index) // Scan Pixel to the left
+            {
+                 setPixelColor(i, Color1);
+            }
+            else // Fading tail
+            {
+                 setPixelColor(i, DimColor(getPixelColor(i)));
+            }
+        }
+        show();
+        Increment();
+    }
+    
+    // Initialize for a Fade
+    void Fade(uint32_t color1, uint32_t color2, uint16_t steps, uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = FADE;
+        Interval = interval;
+        TotalSteps = steps;
+        Color1 = color1;
+        Color2 = color2;
+        Index = 0;
+        Direction = dir;
+    }
+    
+    // Update the Fade Pattern
+    void FadeUpdate()
+    {
+        // Calculate linear interpolation between Color1 and Color2
+        // Optimise order of operations to minimize truncation error
+        uint8_t red = ((Red(Color1) * (TotalSteps - Index)) + (Red(Color2) * Index)) / TotalSteps;
+        uint8_t green = ((Green(Color1) * (TotalSteps - Index)) + (Green(Color2) * Index)) / TotalSteps;
+        uint8_t blue = ((Blue(Color1) * (TotalSteps - Index)) + (Blue(Color2) * Index)) / TotalSteps;
+        
+        ColorSet(Color(red, green, blue));
+        show();
+        Increment();
+    }
+   
+    // Calculate 50% dimmed version of a color (used by ScannerUpdate)
+    uint32_t DimColor(uint32_t color)
+    {
+        // Shift R, G and B components one bit to the right
+        uint32_t dimColor = Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
+        return dimColor;
+    }
+ 
+    // Set all pixels to a color (synchronously)
+    void ColorSet(uint32_t color)
+    {
+        for (int i = 0; i < numPixels(); i++)
+        {
+            setPixelColor(i, color);
+        }
+        show();
+    }
+ 
+    // Returns the Red component of a 32-bit color
+    uint8_t Red(uint32_t color)
+    {
+        return (color >> 16) & 0xFF;
+    }
+ 
+    // Returns the Green component of a 32-bit color
+    uint8_t Green(uint32_t color)
+    {
+        return (color >> 8) & 0xFF;
+    }
+ 
+    // Returns the Blue component of a 32-bit color
+    uint8_t Blue(uint32_t color)
+    {
+        return color & 0xFF;
+    }
+    
+    // Input a value 0 to 255 to get a color value.
+    // The colours are a transition r - g - b - back to r.
+    uint32_t Wheel(byte WheelPos)
+    {
+        WheelPos = 255 - WheelPos;
+        if(WheelPos < 85)
+        {
+            return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+        }
+        else if(WheelPos < 170)
+        {
+            WheelPos -= 85;
+            return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+        }
+        else
+        {
+            WheelPos -= 170;
+            return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+        }
+    }
+};
+ 
+void Ring1Complete();
+//void Ring2Complete();
+//void StickComplete();
+ 
+// Define some NeoPatterns for the two rings and the stick
+//  as well as some completion routines
+NeoPatterns Ring1(35, PIN, NEO_GRB + NEO_KHZ800, &Ring1Complete);
+//NeoPatterns Ring2(16, 6, NEO_GRB + NEO_KHZ800, &Ring2Complete);
+//NeoPatterns Stick(16, 7, NEO_GRB + NEO_KHZ800, &StickComplete);
+
+
+class Sweeper
+{
+  //Servo servo;              // the servo
+  Adafruit_TiCoServo servo;
+  int pos;              // current servo position 
+  int increment;        // increment to move for each interval
+  int  updateInterval;      // interval between updates
+  unsigned long lastUpdate; // last update of position
+  unsigned long lastWaited;
+ 
+public: 
+  Sweeper(int interval)
+  {
+    updateInterval = interval;
+    increment = 1;
+  }
+  
+  void Attach(int pin)
+  {
+    servo.attach(pin);
+  }
+  
+  void Detach()
+  {
+    servo.detach();
+  }
+  
+  void Update()
+  {
+    if(((millis() - lastUpdate) > updateInterval) && ((millis() - lastWaited) > 3000))  // time to update
+    {
+      lastUpdate = millis();
+      pos += increment;
+      servo.write(pos);
+      Serial.println(pos);
+      //moving = true;;
+      digitalWrite(1, HIGH);
+      if ((pos >= 180) || (pos <= 0)) // end of sweep
+      {
+        // reverse direction
+        increment = -increment;
+        lastWaited = millis();
+      }
+    }
+    else {
+      //moving = false;
+      digitalWrite(1, LOW);
+    }
+  }
+};
+
+class Sweeper2
+{
+  //Servo servo;              // the servo
+  Adafruit_TiCoServo servo;
+  int  updateInterval;      // interval between updates
+  
+  volatile int pos;                  // current servo position 
+  volatile unsigned long lastUpdate; // last update of position
+  volatile int increment;            // increment to move for each interval
+ 
+public: 
+  Sweeper2(int interval)
+  {
+    updateInterval = interval;
+    increment = 1;
+  }
+  
+  void Attach(int pin)
+  {
+    servo.attach(pin);
+  }
+  
+  void Detach()
+  {
+    servo.detach();
+  }
+  
+  void reset()
+  {
+    pos = 0;
+    servo.write(pos);
+    increment = abs(increment);
+  }
+  
+  void Update(unsigned long currentMillis)
+  {
+    if((currentMillis - lastUpdate) > updateInterval)  // time to update
+    {
+      lastUpdate = currentMillis;
+      pos += increment;
+      servo.write(pos);
+      if ((pos >= 180) || (pos <= 0)) // end of sweep
+      {
+        // reverse direction
+        increment = -increment;
+      }
+    }
+  }
+};
+
+//digitalWrite(1, LOW);
+
+//Sweeper sweeper1(15);
+//Sweeper2 sweeper1(15);
+
+
+
 void setup() 
 { 
-  servo.attach(9);  //D9ピンをサーボの信号線として設定
-  servo.write(0); // サーボの角度を0°に設定
-  strip.begin();
-  strip.show();
+  Serial.begin(115200);
+  //servo.attach(9);  //D9ピンをサーボの信号線として設定
+  //servo.write(0); // サーボの角度を0°に設定
+  //sweeper1.Attach(9);
+  //OCR0A = 0xAF;
+  //TIMSK0 |= _BV(OCIE0A);
+  pinMode(8, INPUT_PULLUP);
+
+  Ring1.begin(); // initialize pixels
+  //Ring1.TheaterChase(Ring1.Color(255,255,0), Ring1.Color(0,0,50), 1000);
+  //Ring1.RainbowCycle(10);
+  //Ring1.Interval = 2;
+  Ring1.ColorWipe(Ring1.Wheel(random(255)), 2);
+  //Ring1.Color1 = Ring1.Wheel(random(255));
+  //Ring1.Interval = 20;
+  //Ring1.RainbowCycle(10);
+  //strip.begin();
+  //strip.show();
 } 
  
 void loop() 
 { 
-  
+  //sweeper1.Update();
+  Ring1.Update();
+
+  if (digitalRead(8) == LOW) {
+    //Ring1.RainbowCycle(10);
+    //Ring1.Interval = 10;
+    //Ring1.ActivePattern = COLOR_WIPE;
+   // Ring1.Interval = 0;
+    Ring1.ActivePattern = RAINBOW_CYCLE;
+    Ring1.Interval = 5;
+    Ring1.Color1 = Ring1.Wheel(random(255));
+  } else {
+    Ring1.ActivePattern = COLOR_WIPE;
+    Ring1.Interval = 2;
+  }
   //rainbow(5);
-  slowDown(360);
-  slowUp(0);
+  //Ring1.ActivePattern = FADE;
+  //Ring1.Interval = 20;
+  //slowDown(360);
+  //Ring1.ActivePattern = COLOR_WIPE;
+  //slowUp(0);
+  //Ring1.ActivePattern = THEATER_CHASE;
+  //Ring1.Interval = 100;
   //theaterChaseRainbow(25);
   //theaterChase(strip.Color(127, 127, 127), 50);
 }
+
+
+/*
+SIGNAL(TIMER0_COMPA_vect) 
+{
+  unsigned long currentMillis = millis();
+  sweeper1.Update(currentMillis);
+
+} */
 
 void slowDown(int pos){
   while(pos>0){
@@ -31,6 +485,7 @@ void slowDown(int pos){
     pos--;
   }
   delay(3000);
+  //theaterChaseRainbow(25);
 }
 
 void slowUp(int pos){
@@ -39,7 +494,39 @@ void slowUp(int pos){
     delay(15);  
     pos++;
   }
+  //rainbow(20);
   delay(5000); 
+}
+
+void Ring1Complete()
+{/*
+   if (digitalRead(8) == LOW)  // Button #2 pressed
+   {
+        // Alternate color-wipe patterns with Ring2
+        //Ring2.Interval = 40;
+        //Ring1.Color1 = Ring1.Wheel(random(255));
+        //Ring1.Interval = 2000;
+        Ring1.ActivePattern = RAINBOW_CYCLE;
+        Ring1.Interval = 2;
+   }
+   else  // Retrn to normal
+   {
+     Ring1.Reverse();
+   }*/
+}
+
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -95,17 +582,7 @@ void theaterChaseRainbow(uint8_t wait) {
     }
   }
 }
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
+
+
 
 
